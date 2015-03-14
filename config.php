ed76f763a226e114ca6917655a792e12b77cdf2f
wichaksono <wichaksono.w@gmail.com>
2015-03-14 17:41:02 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Nama File : Config.php
+ * File Ini berisi beberapa data penting seperti
+ * Data koneksi ke database
+ * Secret Kode
+ * dan data lain yang nantinya akan digunakan secara terus-menerus
+ */
+ 
+ 
+ 
+ ?>
